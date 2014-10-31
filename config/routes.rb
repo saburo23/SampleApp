6aa4642fcf6958dbbefc53590a44308448ac10cd
xiandong wang <keneethwang@gmail.com>
2014-10-31 13:25:02 -0400

    User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  root "static_pages/home"
+  get "users/new"
+  root "static_pages#home"
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+  get 'signup'  => 'users#new'
+  #get 'contact' => 'static_pages#contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
