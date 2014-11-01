d86018dc1c3ce998c86481c25ee0e1c07930dfa7
xiandong wang <keneethwang@gmail.com>
2014-11-01 15:11:52 -0400

    session and cookie added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   	@user = User.new(user_params)
   	if @user.save
   		#do something
+      log_in @user
       flash[:success] = "Welcome to the app"
       redirect_to @user
   	else
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 SampleApp::Application.routes.draw do
+  get "sessions/new"
   get "users/new"
   root "static_pages#home"
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users
+
   #get 'contact' => 'static_pages#contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
